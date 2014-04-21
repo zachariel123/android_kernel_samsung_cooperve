5924b3103a62ed08e8d9b9726cab5b13b32de44f
bieltv3 <bieltv.3@gmail.com>
2014-04-21 17:20:44 +0200

    Smartass2 governor and other stock governors

--- common/include/linux/cpufreq.h
+++ common/include/linux/cpufreq.h
@@ -372,6 +372,10 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BCM21553)
 extern struct cpufreq_governor cpufreq_gov_bcm;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_bcm)
+/* Extra governors */
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
 #endif
 
 
